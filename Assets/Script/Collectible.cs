b968cf8d478939100313910c36fd4a6fc45982fc
LAPTOPZER <vakadarbhafill@gmail.com>
2025-03-30 15:31:28 +0700

    Collectible + ScoreManager Script

---
+++ Assets/Script/Collectible.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Collectible : MonoBehaviour
+{
+    public int scoreValue = 10; // ค่าคะแนนที่ได้จากการเก็บ
+    public float rotationSpeed = 30f;
+
+    private void Update()
+    {
+        transform.Rotate(0, 0 , rotationSpeed * Time.deltaTime);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("? Collected! + " + scoreValue + " points");
+
+            // เพิ่มคะแนน
+            //ScoreManager.instance.AddScore(scoreValue);
+
+            // ทำลายวัตถุเมื่อถูกเก็บ
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/Script/GoalTrigger.cs
+++ Assets/Script/GoalTrigger.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class GoalTrigger : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    if (other.CompareTag("Player"))
+    //    {
+    //        Debug.Log("Win");
+    //    }
+    //}
+    private void OnCollisionEnter(Collision collision)
     {
-        if (other.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player"))
         {
             Debug.Log("Win");
         }
