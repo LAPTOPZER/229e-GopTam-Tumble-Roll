ffa51afa1d6a286005b91c4b4336eafaf12e99e4
LAPTOPZER <vakadarbhafill@gmail.com>
2025-03-31 00:07:09 +0700

    UI Update

--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class ScoreManager : MonoBehaviour
 {
     public static ScoreManager instance;
     public int score = 0;
     public TMP_Text scoreText;
+    public string sceneName;
+    public TMP_Text winText;
+    public GameObject winPanel;
+    public GameObject creditPanel;
 
     private void Awake()
     {
@@ -14,12 +20,26 @@ public class ScoreManager : MonoBehaviour
         {
             instance = this;
         }
+        winPanel.SetActive(false);
+        creditPanel.SetActive(false);
     }
 
     public void AddScore(int value)
     {
         score += value;
         UpdateScoreUI();
+
+        if (score == 3)
+        {
+            ShowWinScreen();
+        }
+    }
+
+    void ShowWinScreen()
+    {
+        winText.text = "You Win!!";
+        winPanel.SetActive(true);
+        creditPanel.SetActive(true);
     }
 
     private void UpdateScoreUI()
@@ -28,5 +48,9 @@ public class ScoreManager : MonoBehaviour
         {
             scoreText.text = $"Snowman : {score}/3";
         }
+    }
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(sceneName);
     }
 }
