7735301b39dceaabb0f2aada7156dd0a17d54edb
LAPTOPZER <vakadarbhafill@gmail.com>
2025-03-30 21:43:09 +0700

    Edit Player Controller Script

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,14 +1,15 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f;
+    public float sprintMultiplier = 2f;
     public float jumpForce = 5f;
     public Transform cameraTransform;
 
     private Rigidbody rb;
+    private bool isSprinting = false;
 
     void Start()
     {
@@ -18,9 +19,24 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         Move();
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+        {
+            isSprinting = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+        {
+            isSprinting = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
+        {
+            StopMovement();
         }
     }
 
@@ -34,7 +50,9 @@ public class PlayerController : MonoBehaviour
 
         Vector3 moveDirection = (forward * verticalInput).normalized;
 
-        rb.AddForce(moveDirection * moveSpeed, ForceMode.Acceleration);
+        float currentSpeed = moveSpeed * (isSprinting ? sprintMultiplier : 1f);
+
+        rb.AddForce(moveDirection * currentSpeed, ForceMode.Acceleration);
     }
 
     void Jump()
@@ -50,4 +68,9 @@ public class PlayerController : MonoBehaviour
         return Physics.Raycast(transform.position, Vector3.down, 1.1f);
     }
 
+    void StopMovement()
+    {
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+    }
 }
